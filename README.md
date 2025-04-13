a594ebd931187a4e4c68fb67a5d0f0b2284ae934
mgenc2077 <mgenc404@gmail.com>
2025-04-13 23:12:34 +0300

    CH3:Publishers & Queues L1:Exchanges and Queues

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # learn-pub-sub-starter (Peril)
 
 This is the starter code used in Boot.dev's [Learn Pub/Sub](https://learn.boot.dev/learn-pub-sub) course.
+
+To run rabbitMQ:
+```
+./rabbit.sh start
+```
+To run the server:
+```
+go build ./cmd/server && ./server
+```
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic("Failed to connect to RabbitMQ: " + err.Error())
+	}
+	defer conn.Close()
+	channel, err := conn.Channel()
+	if err != nil {
+		panic("Failed to open a channel: " + err.Error())
+	}
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		panic("Failed to publish message: " + err.Error())
+	}
+	fmt.Println("Connected to RabbitMQ")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
